79c01c299f46217a95f651a41c2f1239c61d70bf
sarcasticccccc <abhisheksingh.cs19@gmail.com>
2025-11-15 12:36:28 +0530

    Changes again i made here

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -5,6 +5,19 @@ class Apierror extends Error{
         errors = [],
         stack ="",
     ){
-        super( )
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors
+
+        if(stack){
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this,this.constructor)
+        }
     }
 }
+
+export {Apierror}
